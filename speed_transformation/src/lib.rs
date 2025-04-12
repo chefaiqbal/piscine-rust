e6e965849f8d9af23362fd7559ee6630042cba4a
Amir Iqbal <amiriqbal87@gmail.com>
2025-04-12 16:01:02 +0300

    conversion function

---
+++ speed_transformation/src/lib.rs
@@ -0,0 +1,31 @@
+
+pub fn km_per_hour_to_meters_per_second(km_h: f64) -> f64 {
+    km_h / 3.6
+}
+
+
+
+/*
+Create a function that converts speed from km/h (kilometers per hour) and returns m/s (meters per second).
+Expected Function
+
+pub fn km_per_hour_to_meters_per_second(km_h: f64) -> f64 {
+}
+
+Usage
+
+use speed_transformation::*;
+
+fn main() {
+    let km_h = 100.0;
+    let m_s = km_per_hour_to_meters_per_second(km_h);
+    println!("{} km/h is equivalent to {} m/s", km_h, m_s);
+}
+
+And its output:
+
+$ cargo run
+100 km/h is equivalent to 27.77777777777778 m/s
+$
+
+*/
