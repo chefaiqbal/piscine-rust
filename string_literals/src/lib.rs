6bf60861b870e598023b29cceb0f463285414e35
Amir Iqbal Mohamed <128257436+chefaiqbal@users.noreply.github.com>
2025-04-14 10:32:58 +0000

    Used .unwarp() method

--- string_literals/src/lib.rs
+++ string_literals/src/lib.rs
@@ -15,9 +15,9 @@ pub fn split_at(v: &str, index: usize) -> (&str, &str) {
     v.split_at(index)
 }
 
-//pub fn find(v: &str, pat: char) -> usize {
-//    v.find(pat)
-//}
+pub fn find(v: &str, pat: char) -> usize {
+    v.find(pat).unwrap()
+}
 
 /*
 Instructions
