64842cc6375a23d50274110178aa0ac1677d5fe7
Amir Iqbal Mohamed <128257436+chefaiqbal@users.noreply.github.com>
2025-04-17 04:51:12 +0000

    FIx

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
 
-pub fn word_frequency_counter(words: Vec<&str>) -> HashMap<&str, usize> {
-    words.into_iter().fold(HashMap::new(), |mut acc, word| {
-        *acc.entry(word).or_insert(0) += 1;
-        acc
+pub fn word_frequency_counter<'a>(words: &[&'a str]) -> HashMap<&'a str, usize> {
+    words.iter().copied().fold(HashMap::new(), |mut hash, word| {
+        *hash.entry(word).or_insert(0) += 1;
+        hash
     })
 }
 
 pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
-    frequency_count.values().filter(|&&value| value == 1).count()
+    frequency_count.len()
 }
