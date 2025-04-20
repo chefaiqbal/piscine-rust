54f2bdfbff9ee091ac0867dafa837dfa8f969ab9
Amir Iqbal Mohamed <128257436+chefaiqbal@users.noreply.github.com>
2025-04-20 06:51:24 +0000

    Profanity Checker

---
+++ profanity_filter/src/lib.rs
@@ -0,0 +1,41 @@
+pub fn check_ms(message: &str) -> Result<&str, &str> {
+    let blocked_words = ["stupid", "dumb", "idiot", "fool"];
+
+    if message.is_empty() || blocked_words.iter().any(|&word| message.contains(word)) {
+        Err("ERROR: illegal")
+    } else {
+        Ok(message)
+    }
+}
+
+/*
+Instructions
+Generally it is more desirable to catch the failure of some parts of a program instead of panicking.
+
+For this exercise you will have to create a message blocker, where you must block the word "stupid".
+
+You will need to create a function named check_ms which accepts a string and returns a Result. It should return an error with the message "ERROR: illegal" if the message is either empty or contains the word stupid. Otherwise, it should return the content of the message.
+
+Expected Function
+pub fn check_ms(message: &str) -> Result<&str, &str> {
+    todo!()
+}
+Usage
+Here is a program to test your function
+
+use profanity_filter::*;
+
+fn main() {
+    ["hello there", "", "you are stupid", "stupid"]
+        .into_iter()
+        .for_each(|m| println!("{:?}", profanity_filter::check_ms(m)));
+}
+And its output:
+
+$ cargo run
+Ok("hello there")
+Err("ERROR: illegal")
+Err("ERROR: illegal")
+Err("ERROR: illegal")
+$
+*/
