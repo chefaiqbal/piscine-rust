cb3e1dc680058977c44269d0a5881d8223682dab
Amir Iqbal Mohamed <128257436+chefaiqbal@users.noreply.github.com>
2025-04-21 14:25:47 +0000

    Fix

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -1,4 +1,5 @@
 use json::JsonValue;
+
 pub struct Food {
     pub name: String,
     pub calories: [String; 2],
@@ -8,29 +9,35 @@ pub struct Food {
     pub nbr_of_portions: f64,
 }
 
-pub fn calculate_macros(foods: Vec<Food>) -> JsonValue {
+pub fn calculate_macros(foods: &[Food]) -> JsonValue {
     let mut res = JsonValue::new_object();
     let mut cals: f64 = 0.0;
     let mut carbs: f64 = 0.0;
     let mut proteins: f64 = 0.0;
     let mut fats: f64 = 0.0;
-	let round = 10_f64.powi(2);
+    let round = 10_f64.powi(2); 
 
     for food in foods {
-        let x = food.calories[1]
+
+        let kcal = food.calories[1]
             .chars()
-            .filter(|&c| c.is_numeric()||c == '.')
+            .filter(|&c| c.is_numeric() || c == '.')
             .collect::<String>()
             .parse::<f64>()
             .unwrap();
-        cals = cals + x * food.nbr_of_portions;
-        carbs = carbs + (food.carbs * food.nbr_of_portions);
-        fats = fats + (food.fats * food.nbr_of_portions);
-        proteins = proteins + (food.proteins * food.nbr_of_portions);
+
+        
+        cals += kcal * food.nbr_of_portions;
+        carbs += food.carbs * food.nbr_of_portions;
+        fats += food.fats * food.nbr_of_portions;
+        proteins += food.proteins * food.nbr_of_portions;
     }
-    res.insert("cals", (cals*round).round()/round).unwrap();
-    res.insert("carbs", (carbs*round).round()/round).unwrap();
-    res.insert("proteins", (proteins*round).round()/round).unwrap();
-    res.insert("fats", (fats*round).round()/round).unwrap();
+
+    // Insert rounded values into the JSON object
+    res.insert("cals", (cals * round).round() / round).unwrap();
+    res.insert("carbs", (carbs * round).round() / round).unwrap();
+    res.insert("proteins", (proteins * round).round() / round).unwrap();
+    res.insert("fats", (fats * round).round() / round).unwrap();
+
     res
 }
