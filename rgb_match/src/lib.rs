8099d55bb665c01c6798a2f8cc26d17d6874f59f
Amir Iqbal Mohamed <128257436+chefaiqbal@users.noreply.github.com>
2025-04-23 08:07:02 +0000

    Condition swap()

---
+++ rgb_match/src/lib.rs
@@ -0,0 +1,90 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+
+impl Color {
+    pub fn swap(mut self, first: u8, second: u8) -> Color {
+        for channel in [&mut self.r, &mut self.g, &mut self.b, &mut self.a] {
+            if *channel == first {
+                *channel = second;
+            } else if *channel == second {
+                *channel = first;
+            }
+        }
+        self
+    }
+}
+
+
+/*
+Instructions
+Implement the struct Color with the associated function swap. This function returns a Color with the matching values swapped.
+
+Expected functions
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+
+impl Color {
+    pub fn swap(mut self, first: u8, second: u8) -> Color {
+
+    }
+}
+Usage
+Here is a program to test your function.
+
+use rgb_match::*;
+
+fn main() {
+    let c = Color {
+        r: 255,
+        g: 200,
+        b: 10,
+        a: 30,
+    };
+
+    println!("{:?}", c.swap(c.r, c.b));
+    println!("{:?}", c.swap(c.r, c.g));
+    println!("{:?}", c.swap(c.r, c.a));
+    println!();
+    println!("{:?}", c.swap(c.g, c.r));
+    println!("{:?}", c.swap(c.g, c.b));
+    println!("{:?}", c.swap(c.g, c.a));
+    println!();
+    println!("{:?}", c.swap(c.b, c.r));
+    println!("{:?}", c.swap(c.b, c.g));
+    println!("{:?}", c.swap(c.b, c.a));
+    println!();
+    println!("{:?}", c.swap(c.a, c.r));
+    println!("{:?}", c.swap(c.a, c.b));
+    println!("{:?}", c.swap(c.a, c.g));
+}
+And its output:
+
+$ cargo run
+Color { r: 10, g: 200, b: 255, a: 30 }
+Color { r: 200, g: 255, b: 10, a: 30 }
+Color { r: 30, g: 200, b: 10, a: 255 }
+
+Color { r: 200, g: 255, b: 10, a: 30 }
+Color { r: 255, g: 10, b: 200, a: 30 }
+Color { r: 255, g: 30, b: 10, a: 200 }
+
+Color { r: 10, g: 200, b: 255, a: 30 }
+Color { r: 255, g: 10, b: 200, a: 30 }
+Color { r: 255, g: 200, b: 30, a: 10 }
+
+Color { r: 30, g: 200, b: 10, a: 255 }
+Color { r: 255, g: 200, b: 30, a: 10 }
+Color { r: 255, g: 30, b: 10, a: 200 }
+
+$
+*/
