afcf98816184dc10410e2af86e14042ab718c23d
Amir Iqbal Mohamed <128257436+chefaiqbal@users.noreply.github.com>
2025-05-05 04:51:32 +0000

    <'Lifetimes>

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,49 @@
+#[derive(Debug)]
+pub struct Person<'a> {
+	pub name: &'a str,
+	pub age:&'a u8,
+}
+
+impl <'a> Person<'a>{
+	pub fn new(name: &'a str) -> Person<'a> {
+        Person {
+            name,
+            age: &0,
+        }
+	}
+}
+
+
+/*
+Instructions
+Complete the Person struct with the fields and associated function described below. new should set the age to 0.
+
+Expected Functions and Data Structures (Both need to be completed)
+#[derive(Debug)]
+pub struct Person{
+	pub name: &str,
+	pub age: u8,
+}
+
+impl Person {
+	pub fn new(name: &str) -> Person {
+	}
+}
+Usage
+Here is a program to test your function.
+
+use lifetimes::*;
+
+fn main() {
+	let person = Person::new("Leo");
+
+	println!("Person = {:?}", person);
+}
+And its output:
+
+$ cargo run
+Person = Person { name: "Leo", age: 0 }
+$
+Notions
+lifetimes
+*/
