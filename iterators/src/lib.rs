e82234880d79f40ae2ed555d42125a4d6f2f0e41
Amir Iqbal Mohamed <128257436+chefaiqbal@users.noreply.github.com>
2025-05-08 04:50:36 +0000

    iterators

---
+++ iterators/src/lib.rs
@@ -0,0 +1,32 @@
+#[derive(Copy, Clone)]
+pub struct Collatz {
+    pub v: u64,
+}
+
+impl Collatz {
+    pub fn new(aux: u64) -> Self {
+        Collatz { v: aux }
+    }
+}
+
+impl Iterator for Collatz {
+    type Item = Collatz;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.v <= 1 {
+            None
+        } else {
+            let old_value = self.v;
+            self.v = if self.v % 2 == 0 {
+                self.v / 2
+            } else {
+                self.v * 3 + 1
+            };
+            Some(Collatz { v: old_value })
+        }
+    }
+}
+
+pub fn collatz(n: u64) -> usize {
+    Collatz::new(n).count()
+}
